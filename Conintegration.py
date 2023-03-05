cc65a674fb28a97add0539639826b00cd0cb2538
LemoNade2000 <nitroped7@gmail.com>
2023-03-05 20:34:09 +0800

    pairs added

--- Conintegration.py
+++ Conintegration.py
@@ -7,13 +7,16 @@ from datetime import datetime, timedelta, timezone
 import matplotlib.pyplot as plt
 from statsmodels.tsa.vector_ar.vecm import *
 import random
+import csv
+import os
 
+cd = os.getcwd()
 class TestResult:
     integ = 0
 
 def coint_test(x, y):
-    x_data = np.array(pd.read_csv("{}.csv".format(x)).iloc[:, -1])
-    y_data = np.array(pd.read_csv("{}.csv".format(y)).iloc[:, -1])
+    x_data = np.array(pd.read_csv((cd + "/Data/{}.csv").format(x)).iloc[:, -1])
+    y_data = np.array(pd.read_csv((cd + "/Data/{}.csv").format(y)).iloc[:, -1])
     arr = np.ndarray((len(x_data), 2))
     arr[:, 0] = x_data
     arr[:, 1] = y_data
@@ -23,4 +26,36 @@ def coint_test(x, y):
             return True
     return False
 
-print(coint_test("IVV", "SPY"))
+#print(coint_test("IVV", "SPY"))
+
+ticker_list = []
+with open('TickerList.csv') as f:
+    reader = csv.reader(f)
+    ticker_list = list(reader)
+
+ticker_list = ticker_list[0]
+cointMatrix = pd.DataFrame(index = ticker_list, columns = ticker_list)
+
+for ticker in ticker_list:
+    x_data = np.array(pd.read_csv((cd + "/Data/{}.csv").format(ticker)).iloc[:, -1])
+    if len(x_data) != 251:
+        print(ticker)
+pairs = []
+for i in range(len(ticker_list)):
+    print(i)
+    for j in range(i, len(ticker_list)):
+        if i == j:
+            cointMatrix[ticker_list[i]][ticker_list[j]] = True
+        else:
+            cointMatrix[ticker_list[i]][ticker_list[j]] = coint_test(ticker_list[i], ticker_list[j])
+            cointMatrix[ticker_list[j]][ticker_list[i]] = cointMatrix[ticker_list[i]][ticker_list[j]]
+            if cointMatrix[ticker_list[i]][ticker_list[j]] == True:
+                pairs.append((ticker_list[i], ticker_list[j]))
+
+p = pd.DataFrame(pairs)
+p.to_csv("Pairs.csv")
+
+print(cointMatrix)
+fig, axis = plt.subplots()
+sns.heatmap(cointMatrix.astype(float), annot = False, ax = axis, cmap = "Blues")
+fig.savefig("Heatmap.png")
--- DataCollection.py
+++ DataCollection.py
@@ -5,10 +5,17 @@ from pandas_datareader import data as pdr
 import seaborn as sns
 import datetime as dt
 import matplotlib.pyplot as plt
+import csv
 sns.set_style("darkgrid")
 plt.rcParams.update({'figure.max_open_warning': 0})
 
-ticker_list = ["TSLA", "AAPL", "GS", "A", "SPY", "IVV"]
+
+with open('TickerList.csv') as f:
+    reader = csv.reader(f)
+    ticker_list = list(reader)
+
+print(ticker_list)
+
 def get_data(ticker_list):
     for ticker in ticker_list:
         t = yf.Ticker(ticker)
@@ -17,4 +24,4 @@ def get_data(ticker_list):
         np.log(Close).to_csv("{}.csv".format(ticker))
         #and so on for each ticker --> csv file
 
-get_data(ticker_list)
+get_data(ticker_list[0])
